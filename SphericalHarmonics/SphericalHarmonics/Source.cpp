e6a5b55768fa8adbfe41de90b8a77a3013ca8a01
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2024-05-17 09:20:05 +0300

    added ALGLIB headers and source files

---
+++ SphericalHarmonics/SphericalHarmonics/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+double factorial(int n)
+{
+	double result = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+	}
+	return result;
+}
+
+double LegendrePolynomial(int n, double x)
+{
+	return 0;
+}
+
+double SphericalHarmonic(int l, int m, double theta, double phi)
+{
+	return 0;
+}
+
+int main()
+{
+	return 0;
+}
